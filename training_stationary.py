2fe4a554f6d10ae9d10bbe9863865c1e170c8ce9
peter wang <peterwang724@gmail.com>
2018-10-30 11:38:33 -0400

    major changes

--- training_stationary.py
+++ training_stationary.py
@@ -1,8 +1,7 @@
+import argparse
 import tensorflow as tf
 import numpy as np
 import inputs
-import matplotlib as mpl
-mpl.use('TkAgg')
 import matplotlib.pyplot as plt
 import seaborn as sns
 import os
@@ -16,19 +15,23 @@ import time
 class RNN:
     """An RNN made to model 1D attractor network"""
 
-    def __init__(self, sess, time_steps, path_name, learning_rate, batch = 10):
-        self.x = tf.placeholder(tf.float32, [None, time_steps, state_size], name='input_placeholder')
-        self.y = tf.placeholder(tf.float32, [None, time_steps, state_size], name='output_placeholder')
-        self.init_state = tf.placeholder(tf.float32, [None, state_size], name='init_state')
-        self.sess = sess
-        self.time_steps = time_steps
-        self.path_name = path_name
-        if not os.path.exists(path_name):
-            os.makedirs(path_name)
+    def __init__(self, sess, opts):
+        time_steps = opts.time_steps
+        state_size = opts.state_size
+        save_path = opts.save_path
+        batch_size = opts.batch_size
+        learning_rate = opts.learning_rate
 
-        train_dataset = tf.data.Dataset.from_tensor_slices((self.x, self.y))
-        train_dataset = train_dataset.shuffle(int(1E6)).batch(tf.cast(batch, tf.int64)).repeat()
-        self.train_iter = train_dataset.make_initializable_iterator()
+        if not os.path.exists(save_path):
+            os.makedirs(save_path)
+
+        self.sess = sess
+        self.x = tf.placeholder(tf.float32, [batch_size, time_steps, state_size], name='input_placeholder')
+        self.y = tf.placeholder(tf.float32, [batch_size, time_steps, state_size], name='output_placeholder')
+
+        data = tf.data.Dataset.from_tensor_slices((self.x, self.y))
+        data = data.shuffle(int(1E6)).batch(tf.cast(batch_size, tf.int64)).repeat()
+        self.train_iter = data.make_initializable_iterator()
         self.next_element = self.train_iter.get_next()
 
         inputs_series = tf.unstack(self.x, axis=1)
@@ -36,19 +39,18 @@ class RNN:
         self.W_h = tf.get_variable('W_h', shape=[3, state_size, state_size])
         self.W_b = tf.get_variable('b', shape=[3, state_size], initializer=tf.constant_initializer(0.0))
 
-        self.state_series = [self.init_state]
+        init_state = tf.constant(0.0, shape=[batch_size, state_size], dtype= tf.float32)
+        state_series = [init_state]
         for i, current_input in enumerate(inputs_series):
-            next_state = self.rnn(self.state_series[-1], current_input, i)
-            self.state_series.append(next_state)
+            next_state = self.rnn(state_series[-1], current_input, i)
+            state_series.append(next_state)
 
-        # states1 = tf.scan(self.rnn, tf.transpose(self.x, [1, 0, 2]), initializer=self.init_state)
-        # self.state_series1 = tf.unstack(states1, axis=0)  # output of states leaves t on zero axis
-        self.logits = [s for s in self.state_series]
+        self.logits = state_series
         self.predictions = [tf.nn.softmax(l) for l in self.logits]
-        self.losses = [tf.nn.softmax_cross_entropy_with_logits_v2(logits=l, labels=labels)
+        losses = [tf.nn.softmax_cross_entropy_with_logits_v2(logits=l, labels=labels)
                   for l, labels in zip(self.logits, labels_series)]
 
-        self.total_loss = tf.reduce_mean(self.losses)
+        self.total_loss = tf.reduce_mean(losses)
         self.train_op = tf.train.AdamOptimizer(learning_rate).minimize(self.total_loss)
         self.saver = tf.train.Saver({"W_h": self.W_h, "W_b":self.W_b})
 
@@ -65,39 +67,44 @@ class RNN:
 
     def rnn(self, h_prev, input, name):
         out = tf.tanh(self.W_b[0] + input + tf.matmul(h_prev, self.W_h[0]),
-                               name='time_{}'.format(name))
+                      name='time_{}'.format(name))
         return out
 
-    def run_training(self, inputs, labels, n_epoch=1000, zero_tanh_diag=False, zero_sig_diag=False):
+    def run_training(self, inputs, labels, opts):
         """
         :param inputs: n x t x d input matrix
         :param labels: n x t x d label matrix
         :return:
         """
+        n_epoch = opts.epoch
+        save_path = opts.save_path
+        file_name = opts.file_name
+
         sess.run(self.train_iter.initializer, feed_dict={self.x: inputs, self.y: labels})
-        total_loss = []
-        for n in range(n_epoch):
+        total_loss, logits = [], []
+
+        for ep in range(n_epoch):
             cur_inputs, cur_labels = sess.run(self.next_element)
-            init_state = np.zeros((cur_inputs.shape[0], state_size))
-            feed_dict = {self.x: cur_inputs, self.y: cur_labels, self.init_state: init_state}
-            states, t_loss, _ = self.sess.run([self.state_series, self.total_loss,
-                                                     self.train_op], feed_dict=feed_dict)
-            if n % 20 == 0:
-                total_loss.append(t_loss)
+            feed_dict = {self.x: cur_inputs, self.y: cur_labels}
+            logits, cur_loss, _ = self.sess.run([self.logits, self.total_loss,
+                                               self.train_op], feed_dict=feed_dict)
+            if ep % 20 == 0:
+                total_loss.append(cur_loss)
+            if (ep+1) % 100 == 0:
+                print('[*] Epoch %d  total_loss=%.2f' % (ep, cur_loss))
 
-            if (n+1) % 100 == 0:
-                print(n, t_loss)
+        self.saver.save(self.sess, os.path.join("./",save_path, file_name))
+        return logits, np.array(total_loss)
 
-        self.saver.save(self.sess, "./" + self.path_name + "/stationary")
-        return states, np.array(total_loss)
-
-    def run_test(self, inputs, labels, checkpoint):
-        """Only one example is input for visualization."""
+    def run_test(self, inputs, labels, opts):
+        """Visualization of trained network."""
+        save_path = opts.save_path
+        file_name = opts.file_name
+        checkpoint = os.path.join('./',save_path, file_name)
         self.saver.restore(sess, checkpoint)
-        init_state = np.zeros((inputs.shape[0], state_size))
-        feed_dict = {self.x: inputs, self.y: labels, self.init_state: init_state}
-        states, pred, loss, total_loss = self.sess.run([self.state_series, self.predictions, self.losses,
-                                                                       self.total_loss], feed_dict=feed_dict)
+        feed_dict = {self.x: inputs, self.y: labels}
+        states, predictions, total_loss = \
+            self.sess.run([self.logits, self.predictions, self.total_loss], feed_dict=feed_dict)
         weights, biases = self.sess.run([self.W_h, self.W_b])
 
         cc = 3
@@ -106,7 +113,7 @@ class RNN:
         fig, ax = plt.subplots(nrows=rr, ncols=cc)
         for i in range(inputs.shape[0]):
             cur_state = [s[i] for s in states]
-            cur_pred = [p[i] for p in pred]
+            cur_pred = [p[i] for p in predictions]
             cur_label = labels[i,:,:]
 
             titles = ['Hidden','Predictions','Labels']
@@ -124,8 +131,8 @@ class RNN:
         fig1, ax = plt.subplots(nrows=2, ncols=3)
         for i,w in enumerate(weights):
             sns.heatmap(w, cmap='RdBu_r', vmin=-1, vmax=1, ax= ax[0,i], cbar = False)
-            ax[0,i].axis('off')
             ax[0,i].set_title('W_' + str(i))
+            ax[0,i].axis('off')
             ax[0,i].axis('equal')
 
         for i,b in enumerate(biases):
@@ -134,45 +141,61 @@ class RNN:
             ax[1,i].set_title('B_' + str(i))
             ax[1, i].axis('image')
 
-        path = self.path_name
-        fig.savefig(path + '/img0.png', bbox_inches='tight')
-        fig1.savefig(path + '/img1.png', bbox_inches='tight')
+        fig.savefig(save_path + '/img0.png', bbox_inches='tight')
+        fig1.savefig(save_path + '/img1.png', bbox_inches='tight')
+
+def arg_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--epoch', type=int, default= int(1e4), help= 'number of epochs')
+    parser.add_argument('--batch_size', type=int, default= 5, help= 'batch size')
+    parser.add_argument('--n_input', type=int, default= 5, help= 'number of inputs')
+    parser.add_argument('--learning_rate', type=int, default= .003, help= 'learning rate')
+    parser.add_argument('--time_steps', type=int, default= 20, help= 'rnn time steps')
+
+    parser.add_argument('--state_size', type=int, default= 20, help= 'size of state')
+    parser.add_argument('--bump_size', type=int, default= 5, help= 'size of bump')
+    parser.add_argument('--bump_std', type=int, default= 2, help= 'std of bump')
+
+    parser.add_argument('--noise', action='store_true', default=False, help='noise boolean')
+    parser.add_argument('--noise_intensity', type=float, default= .25, help= 'noise intensity')
+    parser.add_argument('--noise_density', type=float, default= .5, help= 'noise density')
+
+    parser.add_argument('--velocity', action='store_true', default=False, help='velocity boolean')
+    parser.add_argument('--velocity_start', type=int, default=5, help='velocity start')
+    parser.add_argument('--velocity_gap', type=int, default=5, help='velocity gap')
+
+    parser.add_argument('--save_path', type=str, default='./save_path', help='save folder')
+    parser.add_argument('--file_name', type=str, default='stationary', help='file name within save path')
+    return parser
+
+
+if __name__ == '__main__':
+    parser = arg_parser()
+    opts = parser.parse_args()
+
+
+    with tf.Session() as sess:
+        ### Note that the loss can never be zero, as the first input can never produce the right output when the input
+        ### weights are the identity matrix.
+
+        rnn = RNN(sess, opts)
+        sess.run(tf.global_variables_initializer())
+        X, Y, _ = inputs.create_inputs(opts)
+
+        run = False
+        if run:
+            weights, loss = rnn.run_training(X, Y, opts)
+            fig, ax = plt.subplots(nrows=1, ncols=2)
+            ax[0].set_title('Loss')
+            ax[0].plot(np.squeeze(loss))
+
+        test = True
+        if test:
+            rnn.run_test(X[0:7, :, :], Y[0:7, :, :], opts)
+            plt.show(block=False)
 
 
 
-with tf.Session() as sess:
-    state_size = 20
-    bump_size = 7
-    bump_std = 1.5
-    n_epoch = int(1e4)
-    batch_size = 5
-    n_samples = 1000
-    time_steps = 20
-    learning_rate = .003
-    save_path = 'save_path'
 
-    rnn = RNN(sess, time_steps, save_path, learning_rate, batch_size)
-    sess.run(tf.global_variables_initializer())
-    X, Y, _ = inputs.create_inputs(n_samples, state_size = state_size, time_steps=time_steps,
-                                   bump_size=bump_size, bump_std = bump_std,
-                                   noise= False,
-                                   velocity=False, velocity_start=4, velocity_gap = 5)
-
-    run = True
-    if run:
-        weights, loss = rnn.run_training(X, Y, n_epoch=n_epoch)
-        fig, ax = plt.subplots(nrows=1, ncols=2)
-        ax[0].set_title('Loss')
-        ax[0].plot(np.squeeze(loss))
-
-    test = True
-    if test:
-        load_file = save_path + '/stationary'
-        rnn.run_test(X[0:7, :, :], Y[0:7, :, :], checkpoint=load_file)
-
-    plt.show(block=False)
-    time.sleep(3)
-    ### Note that the loss can never be zero, as the first input can never produce the right output when the input
-    ### weights are the identity matrix.
 
 
